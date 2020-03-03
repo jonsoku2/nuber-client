a003daf9f7a6ffe4af65e5b9bcf626d34b6f2242
jonsoku2 <whdtjr2792@gmail.com>
2020-03-03 12:39:27 +0900

    Updates

---
+++ precache-manifest.841060dee14e5be29d376eceddff3246.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ace2a51e9ee4ac5183becdd8b27c36",
+    "url": "/nuber-client/index.html"
+  },
+  {
+    "revision": "f5fecd9ec8490766160f",
+    "url": "/nuber-client/static/css/2.c829125e.chunk.css"
+  },
+  {
+    "revision": "f5fecd9ec8490766160f",
+    "url": "/nuber-client/static/js/2.f0030136.chunk.js"
+  },
+  {
+    "revision": "2838857628caae41cbb82c76e2988170",
+    "url": "/nuber-client/static/js/2.f0030136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3ab3eced1d4c076197",
+    "url": "/nuber-client/static/js/main.c0f6e501.chunk.js"
+  },
+  {
+    "revision": "c392395adb7f7201de74",
+    "url": "/nuber-client/static/js/runtime-main.22b0fd3b.js"
+  },
+  {
+    "revision": "f1662d11088621fd43b5f95d795d749e",
+    "url": "/nuber-client/static/media/bg.f1662d11.png"
+  }
+]);
